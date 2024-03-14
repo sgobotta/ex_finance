da6275634bd081633e2ea885cab5fd873ec0d867
Santiago Botta <sgobotta@gmail.com>
2024-03-14 00:41:49 -0300

    Adds cafecito button to the header

--- lib/ex_finance_web/components/custom_components.ex
+++ lib/ex_finance_web/components/custom_components.ex
@@ -6,10 +6,15 @@ defmodule ExFinanceWeb.CustomComponents do
 
   def cafecito_button(assigns) do
     ~H"""
-    <a href="https://cafecito.app/sgobotta" rel="noopener" target="_blank">
+    <a
+      href="https://cafecito.app/sgobotta"
+      rel="noopener"
+      target="_blank"
+      class="transition duration-300 hover:scale-[1.1] hover:shadow-lg w-28"
+    >
       <img
-        srcset="https://cdn.cafecito.app/imgs/buttons/button_6.png 1x, https://cdn.cafecito.app/imgs/buttons/button_6_2x.png 2x, https://cdn.cafecito.app/imgs/buttons/button_6_3.75x.png 3.75x"
-        src="https://cdn.cafecito.app/imgs/buttons/button_6.png"
+        srcset="https://cdn.cafecito.app/imgs/buttons/button_2.png 1x, https://cdn.cafecito.app/imgs/buttons/button_2_2x.png 2x, https://cdn.cafecito.app/imgs/buttons/button_2_3.75x.png 3.75x"
+        src="https://cdn.cafecito.app/imgs/buttons/button_2.png"
         alt={gettext("Invite me a coffee in cafecito.app")}
       />
     </a>
