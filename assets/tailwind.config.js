0f0e678c4967e71367a800c5cea7a367a95517cf
Santiago Botta <sgobotta@gmail.com>
2025-10-17 22:37:58 -0300

    Add dark mode shadow in tailwind config

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -87,9 +87,11 @@ module.exports = {
         'inner-md': 'inset 0 8px 8px 0 rgb(107 114 128 / 0.5)',
         'inner-sm': 'inset 0 6px 6px 0 rgb(107 114 128 / 0.5)',
         'inner-xs': 'inset 0 4px 4px 0 rgb(107 114 128 / 0.5)',
+        'dark-inner-xs': 'inset 0 6px 6px 0 rgb(24 24 27 / 1)',
         'outer-md': '0 0 4px 2px rgb(107 114 128 / 0.5)',
         'outer-sm': '0 0 3px 2px rgb(107 114 128 / 0.5)',
         'outer-xs': '0 0 2px 1px rgb(107 114 128 / 0.5)'
+
       },
       colors: {
         brand: "#FD4F00",
